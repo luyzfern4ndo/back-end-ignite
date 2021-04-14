25a8b7e0387bed9095e4e087b9ab1314bcba90bf
Luiz Fernando <luizfernando.etconx@gmail.com>
2021-04-13 23:26:30 -0300

    promise, async/await to read file

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -2,18 +2,48 @@ import csvParse from 'csv-parse';
 import fs from 'fs';
 // file system
 
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
-    const stream = fs.createReadStream(file.path);
+  constructor(private categoriesRepository: ICategoriesRepository) {}
 
-    const parseFile = csvParse();
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
 
-    // pipe: a cada linha lida, pegamos os dados separados por vírgula
-    stream.pipe(parseFile);
+      // por padrão o csvParse identifica que o separador é por vírgula ( , )
+      const parseFile = csvParse();
 
-    parseFile.on('data', async line => {
-      console.log(line);
+      // pipe: a cada linha lida, pegamos os dados separados por vírgula
+      stream.pipe(parseFile);
+
+      parseFile
+        .on('data', async line => {
+          const [name, description] = line;
+
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on('end', () => {
+          resolve(categories);
+        })
+        .on('error', err => {
+          reject(err);
+        });
     });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+    console.log(categories);
   }
 }
 
