0b26012ce8e88be1cd682718a6449830b7cedd1f
Luiz Fernando <luizfernando.etconx@gmail.com>
2021-04-19 15:52:28 -0300

    removing username from User

--- src/modules/accounts/useCases/CreateUser/CreateUserController.ts
+++ src/modules/accounts/useCases/CreateUser/CreateUserController.ts
@@ -5,12 +5,12 @@ import { CreateUserUseCase } from './CreateUserUseCase';
 
 class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { name, username, password, driver_license, email } = request.body;
+    const { name, password, driver_license, email } = request.body;
     const createUserUseCase = container.resolve(CreateUserUseCase);
 
     await createUserUseCase.execute({
       name,
-      username,
+
       password,
       driver_license,
       email,
